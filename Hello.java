4f808efcb22a38e39000be2102c7a0ad0a27283c
imransajeb <imran.ih232@gmail.com>
2024-08-23 20:16:34 +0600

    Change greeting method

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 public class Hello {
   public static void main (String[] args) {
     System.out.println("VS code is Awesome...");
-    greeting("Imran Hossain");
+   
+   greeting("Git");
   }
 
   public static void greeting(String name) {
-   System.out.println("Welcome " . name)
+   System.out.println("Bye Bye " . name)
   }
 }
