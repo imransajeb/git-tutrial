eb1e897f1e8fdbb2d1b03b49ff39a9923f7dc021
imransajeb <imran.ih232@gmail.com>
2024-08-23 20:45:37 +0600

    Remove greting  and add display methd

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ public class Hello {
   public static void main (String[] args) {
     System.out.println("VS code is Awesome...");
    
-   greeting("Git");
+    display();
   }
 
-  public static void greeting(String name) {
-   System.out.println("Bye Bye " . name)
+  public static void display () {
+    System.out.println(Hello Hello);
   }
 }
