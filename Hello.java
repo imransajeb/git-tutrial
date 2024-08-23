3e8c5b2589f8c0006286f5c779f60d8391b6c53d
imransajeb <imran.ih232@gmail.com>
2024-08-23 20:01:33 +0600

    Added a new method called doisplay tp Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
   public static void main (String[] args) {
     System.out.println("VS code is Awesome...");
+    greeting("Imran Hossain");
+  }
+
+  public static void greeting(String name) {
+   System.out.println("Welcome " . name)
   }
 }
